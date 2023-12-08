141f8bb7760c33434bad7d16da543e70a77b2fd0
Erwann Maton <erwann.mutant@gmail.com>
2023-12-08 10:31:41 +0100

    Ajout de la fonction de quitter le jeu

---
+++ scripts/CanvasLayer.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var main: Control = $Main
+
+
+func _on_button_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_quit_pressed():
+	get_tree().quit()
+
+
