eaf1b624be2b673ca5cffdac929f46219cb6f988
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2024-01-03 18:41:34 +0100

    Ajout

--- scripts/Manager/CharacterSelectionManager.gd
+++ scripts/Manager/CharacterSelectionManager.gd
@@ -9,4 +9,6 @@ var opponentScript = preload("res://scripts/Player2.gd")
 var selectableCharacters = {
 	"Sagat" : preload("res://characters/Sagat/Scenes/Sagat.tscn"),
 	"Vega" : preload("res://characters/Vega/Scenes/Vega.tscn"),
+	"Fei_Long" : preload("res://characters/Feilong/Scene/Feilong.tscn"),
+	"Blanka" : preload("res://characters/Blanka/Scenes/Blanka.tscn"),
 }
